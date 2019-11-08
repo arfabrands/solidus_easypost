ed8b631943af52b7ef6fbaeab60d554fc75c752f
Bryan Mahoney <bryan@goDynamo.com>
2019-11-08 12:33:29 -0500

    Add tracking_url to the migrations on install

--- db/migrate/20140515024440_add_easy_post_fields_to_shipping_rate.rb
+++ db/migrate/20140515024440_add_easy_post_fields_to_shipping_rate.rb
@@ -6,5 +6,6 @@ class AddEasyPostFieldsToShippingRate < SolidusSupport::Migration[4.2]
     add_column :spree_shipping_rates, :easy_post_shipment_id, :string
     add_column :spree_shipping_rates, :easy_post_rate_id, :string
     add_column :spree_shipments, :label, :string
+    add_column :spree_shipments, :tracking_url, :string
   end
 end
