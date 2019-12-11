a9296b92cd8da6e187dc9456937b3097b1a9c297
Bryan Mahoney <bryan@goDynamo.com>
2019-12-11 07:46:07 -0500

    WIP on free shipping support

--- app/decorators/solidus_easypost/spree/order_decorator.rb
+++ app/decorators/solidus_easypost/spree/order_decorator.rb
@@ -6,7 +6,11 @@ module SolidusEasypost
 
       # To implement in host app
       def eligible_for_free_shipping?
-        false
+        total >= free_shipping_threshold
+      end
+
+      def free_shipping_threshold
+        ENV.fetch('FREE_SHIPPING_THRESHOLD', 1000000)
       end
 
       ::Spree::Order.prepend self
--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -4,9 +4,13 @@ module Spree
   RSpec.describe Order, type: :model do
     context "free shipping" do
       let(:order) { Spree::Order.create }
-      it "should qualify for free shipping if total exceeds 25" do
+      it "should not qualify for free shipping by default" do
         order.total = 20
         expect(order.eligible_for_free_shipping?).to be false
+      end
+      
+      it "should not qualify for free shipping if total exceeds shipping threshold" do
+        allow(ENV).to receive(:[]).with('FREE_SHIPPING_THRESHOLD').and_return(25)
         order.total = 25
         expect(order.eligible_for_free_shipping?).to be true
       end
